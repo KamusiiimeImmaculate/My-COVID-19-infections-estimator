4b80df016cf67bd890e68595f80a785b7aa5dc0b
Kamusiime Immaculate <63217020+KamusiiimeImmaculate@users.noreply.github.com>
2020-04-13 16:03:24 +0300

    forth one

--- infectionsByRequestedTime.py
+++ infectionsByRequestedTime.py
@@ -3,7 +3,8 @@ a = "reportedCases"
 c = "projectednumberofinfectionsafter28days"
 d = "impact.currentlyInfected"
 e = "severeImpact.currentlyInfected"
-
+f = "severeCasesByRequestedTime"
+g = "casesForVentilatorsByRequestedTime"
 
 currentlyInfected = "a * 10"
 
@@ -11,3 +12,6 @@ b = "currentlyInfected"
 
 c = "b *(2 to the power of 9)"  #currentlyInfected x 512
 e = "c *50"
+f = "15% * e"
+g = "2% * e"
+g = h
