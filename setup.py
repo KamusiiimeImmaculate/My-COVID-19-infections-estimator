7c1d11e91f15d3989784424b4300eef31003f1aa
Kamusiime Immaculate <63217020+KamusiiimeImmaculate@users.noreply.github.com>
2020-04-13 13:17:17 +0300

    second commit

--- setup.py
+++ setup.py
@@ -1,3 +1,43 @@
+
+{
+"data":{
+    "region": {
+        "name": "Africa",
+        "AvgAge": 19.7,
+        "avgDailyIncomeInUSD":4,
+        "avgDailyIncomePopulation": 0.73
+         },
+    "periodType": "days",
+    "timeToElapse": 38,
+    "reportedCases": 2747,
+    "population": 92931687
+     },
+"estimate": {
+    "impact":{
+        "currentlyInfected": 27470,
+        "infectionByRequestedTime":112517120,
+        "severeCasesByRequestedTime": 16877562,
+        "hospitalBedsByRequestedTIME": -16639962,
+        "casesForICUByRequestdTime": 2250342,
+        "casesForVentilatorBrRequestedTime": 2250342,
+        "dollarsFlight": 12484899635.2
+        
+    },
+    "severeImpact": {
+        "currentlyInfected": 137350,
+        "infectedByRequestedTime": 5625856,
+        "severeCasesByRequestedTime": 84387840,
+        "hospitalBedsByRequesstedTime": -84150234,
+        "casesForICUByRequestedTime": 28129280,
+        "casesForVentilatorsByRequestedTime": 11251712,
+        "dollarsInFlight": 62424498176
+    }
+} 
+}
+
+
+msg = "Hello World"
+print (msg)
 from setuptools import setup, find_packages
 
 
